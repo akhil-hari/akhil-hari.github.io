5a3d57e5f4c9a560bd4b512034b03aa2149f17c1
Akhil H Nair <aklhnrpm@gmail.com>
2022-10-25 12:37:25 +0530

    uu

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 //alert('The page is linked to main.js')
+var states = {skills:"skills", about:""}
+var state = "about"
+function switchState(next_state){
+    state = next_state;
+    window.history.pushState({},'', `/${states[next_state]}`);
+}
